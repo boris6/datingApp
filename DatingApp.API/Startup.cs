0a70a659a4723e7e18096ccc9d5ab612717df43e
Boris Sijan <sijanb@outlook.com>
2019-12-18 21:44:23 +0100

    Add http requests

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +31,8 @@ namespace DatingApp.API
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseRouting();
+
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
